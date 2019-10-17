0e099beb75f329cbf5a1b12d7987e0290ab1bfd5
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-10-16 21:50:54 -0400

    Getting closer to working sidebar

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import NavMenu from './NavMenu';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import NavMenu from "./NavMenu";
+import Sidebar from "./Sidebar";
 
 export default props => (
-  <Container fluid>
-    <Row>
-      <Col sm={3}>
-        <NavMenu />
-      </Col>
-      <Col sm={9}>
-        {props.children}
-      </Col>
-    </Row>
-  </Container>
+  <>
+    <Container fluid>
+      <Row>
+        <Col sm={3} className="px-0">
+          <NavMenu />
+          {/* <Sidebar /> */}
+        </Col>
+        <Col sm={9}>{props.children}</Col>
+      </Row>
+    </Container>
+  </>
 );
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,36 +1,36 @@
-﻿import React from 'react';
-//import { Link, NavLink } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-//import Nav from 'react-bootstrap';
-//import { LinkContainer } from 'react-router-bootstrap';
-import './NavMenu.css';
+﻿import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import "./NavMenu.css";
 
+// <Navbar /* fixed="top" */ collapseOnSelect={true} bg="light">
 export default props => (
-    <Navbar fixed="top" collapseOnSelect={true}>
-        <Navbar.Brand>
-          <Nav.Link to="/">Freelance</Nav.Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-            <Nav>
-                <Nav.Item>
-                  <Nav.Link href="/">
-                    Home
-                  </Nav.Link>
-                </Nav.Item>
-        
-                <Nav.Item>
-                  <Nav.Link to="/counter">
-                    Counter
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link to="/fetchjobs">
-                    Jobs
-                  </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar> 
+  <Navbar
+    className="flex-column"
+    bg="dark"
+    variant="dark"
+    collapseOnSelect={true}
+    expand="xl"
+  >
+    {/* <Nav.Link to="/">Freelance</Nav.Link> */}
+    <Navbar.Brand href="/">Freelance</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav defaultActiveKey="/" className="flex-column mx-auto">
+        <Nav.Item>
+          <Link to="/">Home</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/counter">Counter</Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link to="/fetchjobs">Jobs</Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
