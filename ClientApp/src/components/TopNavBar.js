cb580d263cf9b597a4e75c6d58f0c27e8c55de7e
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-11-03 13:44:23 -0500

    Active nav applied correctly now

--- ClientApp/src/components/TopNavBar.js
+++ ClientApp/src/components/TopNavBar.js
@@ -3,27 +3,27 @@ import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import { LinkContainer } from "react-router-bootstrap";
+import { IndexLinkContainer } from "react-router-bootstrap";
 
 export default props => (
   <Navbar bg="dark" variant="dark">
     <Container>
       <Navbar.Brand href="/">Freelance</Navbar.Brand>
-      <Nav variant="pills" className="mr-auto">
+      <Nav className="mr-auto">
         <Nav.Item>
-          <LinkContainer to="/">
+          <IndexLinkContainer to="/">
             <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
+          </IndexLinkContainer>
         </Nav.Item>
         <Nav.Item>
-          <LinkContainer to="/counter">
+          <IndexLinkContainer to="/counter">
             <Nav.Link>Counter</Nav.Link>
-          </LinkContainer>
+          </IndexLinkContainer>
         </Nav.Item>
         <Nav.Item>
-          <LinkContainer to="/fetchjobs">
+          <IndexLinkContainer to="/fetchjobs">
             <Nav.Link>Jobs</Nav.Link>
-          </LinkContainer>
+          </IndexLinkContainer>
         </Nav.Item>
       </Nav>
     </Container>
