b3dfce367198053d18a43ed5fe5cb322fd2e56d1
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2020-05-27 17:02:57 -0400

    Deleted unnecessary App file

--- Web/ClientApp/src/App.js
+++ Web/ClientApp/src/App.js
@@ -1,21 +1,45 @@
-﻿import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+//import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import configureStore from "./store/configureStore";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Counter from "./components/Counter";
 import FetchJobs from "./components/FetchJobs";
 
-// This component not imported anymore, can be deleted? Or save the App.test.js file for unit testing?
+// Create browser history to use in the Redux store
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const history = createBrowserHistory({ basename: baseUrl });
+
+// Get the application-wide store instance, prepopulating with state from the server where available.
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
+
+const rootElement = document.getElementById("root");
+
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Route exact path="/" component={Home} />
-        <Route path="/counter" component={Counter} />
-        <Route path="/fetchjobs/:startDateIndex?" component={FetchJobs} />
-      </Layout>
+      <Provider store={store}>
+        <Router>
+          <Layout>
+            <Route exact path="/" component={Home} />
+            <Route path="/counter" component={Counter} />
+            {/* <Route path="/jobs/:startDateIndex?" component={FetchJobs} /> */}
+            <Route path="/jobs" component={FetchJobs} />
+          </Layout>
+        </Router>
+      </Provider>
     );
   }
 }
 
-export default App;
+ReactDOM.render(<App />, rootElement);
+
+registerServiceWorker();
