a66e9c51c57b7d5fcd50919d4dde51be5c175c57
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2020-05-29 16:42:10 -0400

    Removed some unused code

--- Web/ClientApp/src/components/JobDetail.js
+++ Web/ClientApp/src/components/JobDetail.js
@@ -21,47 +21,6 @@ const JobDetail = (props) => {
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>{jobDetail.jobTitle}</Modal.Header>
         <Modal.Body>this is the body</Modal.Body>
-        {/* <Table responsive>
-          <thead>
-            <tr>
-              <th>Employer</th>
-              <th>Job Title</th>
-              <th>Location</th>
-              <th>Compensation</th>
-              <th>Start Date</th>
-              <th>End Date</th>
-              <th>Paid</th>
-              <th>Date Invoiced</th>
-              <th>Rate</th>
-              <th>Hours Worked</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {jobList.map((job) => (
-              <tr key={job.id}>
-                <td>{job.employer}</td>
-                <td>{job.jobTitle}</td>
-                <td>{job.location}</td>
-                <td>
-                  {new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  }).format(job.compensation)}
-                </td>
-                <td>{new Date(job.startDate).toLocaleDateString()}</td>
-                <td>{new Date(job.endDate).toLocaleDateString()}</td>
-                <td>{job.paid ? "Yes" : "No"}</td>
-                <td>{new Date(job.dateInvoiced).toLocaleDateString()}</td>
-                <td>{job.rate}</td>
-                <td>{job.hoursWorked}</td>
-                <td>
-                  <Link to={`/jobs/${job.id}`}>Details</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table> */}
       </Modal>
     </>
   );
